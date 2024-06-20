149c7fc8049308be7af5fb8af2bb1d9061dc1717
Dharam Soni <dharamsoni1010@gmail.com>
2024-06-20 22:30:44 +0530

    Updated index.dart

--- routes/index.dart
+++ routes/index.dart
@@ -1,5 +1,8 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  return Response(body: 'Welcome to Dart Frog!');
+  return Response(
+    body:
+        'Welcome to Nimbus API! To get a screenshot of a tweet use /twitter?screenshot=<tweet_url>',
+  );
 }
